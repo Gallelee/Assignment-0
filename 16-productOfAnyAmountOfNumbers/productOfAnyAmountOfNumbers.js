4df7c7ffa49c268bed05e4e7306b629dbf344479
Gallelee <thimotus@gmail.com>
2022-01-01 14:57:40 -0500

    Excersises Complete

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let count = 1
+  for(let x of args){
+    count=x*count
+  }
+  return count
 }
 
 // Do not edit this line;
