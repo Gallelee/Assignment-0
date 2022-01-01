a0a8cc78b497b6e5550badc7b30c5a54ce54325f
Gallelee <thimotus@gmail.com>
2022-01-01 15:40:39 -0500

    Resolved changes

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,20 +1,8 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
-<<<<<<< HEAD
-<<<<<<< HEAD
-  let count = 1
-  for(let x of args){
-    count=x*count
-=======
   let count = 1 
   for(let x of args){
     count = count * x
->>>>>>> 0d8ac694d4438f87f3fbf45d299b2d1989871b0b
-=======
-  let count = 1 
-  for(let x of args){
-    count = count * x
->>>>>>> 0d8ac694d4438f87f3fbf45d299b2d1989871b0b
   }
   return count
 }
