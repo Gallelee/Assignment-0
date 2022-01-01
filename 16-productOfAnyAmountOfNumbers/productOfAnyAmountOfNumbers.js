0d8ac694d4438f87f3fbf45d299b2d1989871b0b
Gallelee <92611336+Gallelee@users.noreply.github.com>
2022-01-01 15:03:35 -0500

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  let count = 1 
+  for(let x of args){
+    count = count * x
+  }
+  return count
 }
 
 // Do not edit this line;
